fd0a5133da7cbef9fbb3cc14a47fecf4a78f0512
Nadyaulia <112957809+Nadyaulia@users.noreply.github.com>
2025-06-14 19:31:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -95,6 +95,7 @@ def preprocess_input(data):
     data['SCC'] = data['SCC'].map(scc_map).fillna(-1).astype(int)
     data['CAEC'] = data['CAEC'].map(caec_map).fillna(-1).astype(int)
     data['MTRANS'] = data['MTRANS'].map(mtrans_map).fillna(-1).astype(int)
+    data['family_history_with_overweight'] = data['family_history_with_overweight'].map(family_history_map).fillna(-1).astype(int)
 
     # Normalisasi fitur numerik
     numerical_features = ['Age', 'Height', 'Weight', 'FCVC', 'NCP', 'CH2O', 'FAF', 'TUE']
